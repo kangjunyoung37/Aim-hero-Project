7d6a0b74c200c15398c4c1e10f7c44d9be47613c
kangjunyoung37 <rkdwnsdud37@gmail.com>
2022-05-10 22:16:25 +0900

    #타켓 랜덤으로 움직이기

--- Aim hero/Assets/Script/TargetController.cs
+++ Aim hero/Assets/Script/TargetController.cs
@@ -7,11 +7,13 @@ public class TargetController : MonoBehaviour
 
     private TargetMovement targetMovement;
 
+    [SerializeField]
+    private Transform target;
     
-    private Vector3 movespeed;
-    
-    private int a;
-    private float MoveTime = 3.0f;
+    private Vector3 movespeed = new Vector3(7, 0, 0);
+
+    private float MoveTime = 2.0f;
+    private float LastTime = 0;
    
     private void Awake()
     {
@@ -19,26 +21,54 @@ public class TargetController : MonoBehaviour
        
     }
 
-    private IEnumerator Right()
+    private Vector3 patern(int num)
+    {
+        switch (num)
+        {
+            case 0:
+                Vector3 right = new Vector3(5, 0, 0);
+            return right;
+                
+            case 1:
+                Vector3 left = new Vector3(-5, 0, 0);
+                return left;
+
+            
+            case 2:
+                Vector3 rightcorss = new Vector3(5, 0, 4);
+                return rightcorss;
+
+            case 3:
+                Vector3 leftcorss = new Vector3(-5, 0, -4);
+                return leftcorss;            
+        }
+        return Vector3.zero;
+        
+    }
+    private IEnumerator Wait()
     {
 
-        yield return new WaitForSeconds(1f);
-        MoveTime += 3.0f;
-        
-
-
+        movespeed = new Vector3(0, 0, 0);
+        yield return new WaitForSeconds(0.5f);
+        LastTime = Time.time;
+  
     }
     private void Update()
     {
-        movespeed = new Vector3(2, 0, 0);
-        if (Time.time < MoveTime)
+        transform.rotation = Quaternion.Euler(0, 0, 90);
+        transform.LookAt(target.position);
+        if (Time.time - LastTime <= MoveTime)
         {
-              
             transform.position += movespeed * Time.deltaTime;
-
+        }
+        else
+        {
+            StartCoroutine(Wait());
+            int randomnum = Random.Range(0, 4);
+            movespeed = patern(randomnum);
         }
         
-        
+
     }
 
 }
