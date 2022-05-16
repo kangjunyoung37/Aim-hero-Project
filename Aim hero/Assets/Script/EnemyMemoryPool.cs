7ec0f330ac140591906b7dd3709415071fe61865
kangjunyoung37 <rkdwnsdud37@gmail.com>
2022-05-16 23:02:15 +0900

    #랜덤한 곳에 타켓생성 구현

---
+++ Aim hero/Assets/Script/EnemyMemoryPool.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMemoryPool : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject enemySpawnPointPrefab;
+    [SerializeField]
+    private GameObject enemyPrefab;
+    [SerializeField]
+    private float enemySpawnTime = 1;
+    [SerializeField]
+    private float enemySpawnLatency = 1;
+
+    private MemoryPool spawnPointMemoryPool;
+    private MemoryPool enemyMemoryPool;
+
+    private Vector2Int mapsize = new Vector2Int(50, 50);
+
+    private void Awake()
+    {
+        spawnPointMemoryPool = new MemoryPool(enemySpawnPointPrefab);
+        enemyMemoryPool = new MemoryPool(enemyPrefab);
+
+        StartCoroutine("SpawnTile");
+    }
+    private IEnumerator SpawnTile()
+    {
+        int currentNumber = 0;
+        int maxiumNumber = 50;
+
+        while (true)
+        {
+            GameObject item = spawnPointMemoryPool.ActivatePoolItem();
+            item.transform.position = new Vector3(Random.Range(-mapsize.x * 0.49f, mapsize.x * 0.49f), 1, Random.Range(-mapsize.y * 0.49f, mapsize.y * 0.49f));
+            StartCoroutine("SpawnEnemy", item);
+            currentNumber++;
+
+            if(currentNumber >= maxiumNumber)
+            {
+                currentNumber = 0;
+
+            }
+            yield return new WaitForSeconds(enemySpawnTime);
+        }
+
+    }
+    private IEnumerator SpawnEnemy(GameObject point)
+    {
+        yield return new WaitForSeconds(enemySpawnLatency);
+
+        GameObject item = enemyMemoryPool.ActivatePoolItem();
+        item.transform.position = point.transform.position;
+
+        spawnPointMemoryPool.DeactivatePoolItem(point);
+    }
+
+}
